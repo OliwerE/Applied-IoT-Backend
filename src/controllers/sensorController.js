b25af6eb60b69430da4a1e3ab22d17a4b996c390
Oliwer <oe222ez@student.lnu.se>
2022-06-17 17:44:29 +0200

    Store sensor values in database

--- src/controllers/sensorController.js
+++ src/controllers/sensorController.js
@@ -2,15 +2,37 @@
  * Sensor controller module
  */
 
+import createError from 'http-errors'
+import { SensorReport } from '../models/sensorReport.js'
+
 /**
  * Sensor controller class.
  */
 export class SensorControler {
-  updateSensors(req, res, next) {
-    console.log(req.body)
+  /**
+   * Creates sensor reports for an array of sensor objects.
+   *
+   * @param {object} req - Request object.
+   * @param {object} res - Response object.
+   * @param {object} next - Next function.
+   */
+  async updateSensors (req, res, next) {
+    try {
+      const sensors = req.body
 
-    // store data
+      for (let i = 0; i < sensors.length; i++) {
+        const sensor = sensors[i]
 
-    res.status(200).send()
+        const newSensorReport = new SensorReport({
+          sensorName: sensor.sensorName,
+          value: sensor.value
+        })
+
+        await newSensorReport.save()
+      }
+      res.status(200).send()
+    } catch (err) {
+      next(createError(500))
+    }
   }
 }
