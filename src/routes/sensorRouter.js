0acbc6f664676e2b6ca3fd52ef5ebcb878adfea4
Oliwer <oe222ez@student.lnu.se>
2022-06-17 17:47:30 +0200

    Add authorization for updateSensors

--- src/routes/sensorRouter.js
+++ src/routes/sensorRouter.js
@@ -10,6 +10,25 @@ export const router = express.Router()
 
 const sensorController = new SensorControler()
 
-router.post('/', sensorController.updateSensors)
+/**
+ * Check if API key is valid.
+ *
+ * @param {object} req - Request object.
+ * @param {object} res - Response object.
+ * @param {object} next - Next function.
+ */
+const authorize = (req, res, next) => {
+  try {
+    if (req.headers.authorization === process.env.API_KEY) {
+      next()
+    } else {
+      next(createError(401))
+    }
+  } catch (err) {
+    next(createError(500))
+  }
+}
+
+router.post('/', authorize, (req, res, next) => sensorController.updateSensors(req, res, next))
 
 router.use('*', (req, res, next) => next(createError(404)))
