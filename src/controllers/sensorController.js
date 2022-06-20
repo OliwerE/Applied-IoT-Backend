e5eb07cc94d5c1340b757ff502baa5d52f2ea64a
Oliwer <oe222ez@student.lnu.se>
2022-06-20 14:08:03 +0200

    Add endpoint: All sensors average value per hour

--- src/controllers/sensorController.js
+++ src/controllers/sensorController.js
@@ -3,6 +3,8 @@
  */
 
 import createError from 'http-errors'
+import moment from 'moment'
+
 import { SensorReport } from '../models/sensorReport.js'
 
 /**
@@ -62,6 +64,86 @@ export class SensorController {
     } catch (err) {
       next(createError(404))
     }
+  }
+
+  /**
+   * Returns all sensors average value per hour over the last 24 hours.
+   *
+   * @param {object} req - Request object.
+   * @param {object} res - Response object.
+   * @param {object} next - Next function.
+   */
+  async getAllSensorsDayAvgByHour (req, res, next) {
+    try {
+      const intervalStart = new Date()
+      intervalStart.setUTCHours(intervalStart.getUTCHours(), 0, 0, 0)
+
+      const responseObj = {}
+
+      const sensorNames = await SensorReport.find().distinct('sensorName')
+
+      for (let i = 0; i < sensorNames.length; i++) { // Add empty array for each sensor in response obj.
+        responseObj[sensorNames[i]] = []
+      }
+
+      for (let i = 0; i < 24; i++) {
+        intervalStart.setHours(intervalStart.getHours() - 1) // skip current hour, beacuse not final value.
+
+        const intervalEnd = new Date(intervalStart)
+        intervalEnd.setUTCHours(intervalStart.getUTCHours(), 59, 59, 999)
+
+        for (let a = 0; a < sensorNames.length; a++) {
+          const hoursAgo = i + 1
+          const avg = await this.#getSensorAvgFromInterval(intervalStart, intervalEnd, sensorNames[a], hoursAgo)
+
+          if (avg.length === 1) {
+            responseObj[sensorNames[a]].push({
+              hoursAgo,
+              value: avg[0].value
+            })
+          } else {
+            responseObj[sensorNames[a]].push({
+              hoursAgo,
+              value: null
+            })
+          }
+        }
+      }
+
+      res.json({ msg: 'Average values per hour from all sensors over the last 24 hours.', sensors: responseObj })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  /**
+   * Returns average sensor value of specified interval.
+   *
+   * @param {Date} intervalStart - Interval start.
+   * @param {Date} intervalEnd - Interval end.
+   * @param {string} sensorName - sensor name.
+   * @param {number} timeAgo - Age of average value.
+   */
+  async #getSensorAvgFromInterval (intervalStart, intervalEnd, sensorName, timeAgo) {
+    const res = await SensorReport.aggregate([
+      {
+        $match: {
+          sensorName,
+          createdAt: {
+            $gte: intervalStart,
+            $lt: intervalEnd
+          }
+        }
+      },
+      {
+        $group:
+        {
+          _id: timeAgo,
+          value: { $avg: '$value' }
+        }
+      }
+    ])
+    return res
   }
 
   /**
